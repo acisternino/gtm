c83fcb6d628428f00e20bb7792c0fd821113c8c8
Andrea Cisternino <a.cisternino@gmail.com>
2017-10-13 23:04:36 +0200

    Implement split Add functions

--- ctree/ctree.go
+++ ctree/ctree.go
@@ -10,10 +10,10 @@ This implementation is heavily inspired by the original implementation in
 Tierra and is tailored for managing memory segments. A characteristic of this
 implementation is that two constraints are always true.
 
-The length of a node is always equal or larger to that of its children.
+1. The length of a node is always equal or larger to that of its children.
 
-The address of a node is always larger or equal to those of its entire left
-subtree. Resp. smaller than those of its right subtree.
+2. The address of a node is always larger or equal to those of its entire
+   left subtree. Resp. smaller than those of its right subtree.
 
 From rule 1 derives that the root of the tree always contains the node with
 the greates length. This characteristic is useful when using ctree to store
@@ -29,12 +29,49 @@ import (
 	"github.com/pkg/errors"
 )
 
+//---- Frame -------------------------------------------------------------
+
 // Frame represents a segment of free memory.
 type Frame struct {
 	Address int32
 	Length  int32
 	left    *Frame
 	right   *Frame
+}
+
+// Add adds a new Frame to the subtree anchored at this Frame.
+func (f *Frame) Add(nf, parent *Frame) {
+	current := f
+	loop := true
+
+	for loop {
+		if current != nil && nf.Length <= current.Length {
+			// new frame is smaller than current, descend according to address
+			parent = current
+			if nf.Address <= current.Address {
+				current = current.left
+			} else {
+				current = current.right
+			}
+		} else {
+			// new frame is larger than current or we reached the end: insert here
+			if nf.Address <= parent.Address {
+				// append left
+				nf.left = current // was parent.left
+				nf.right = nil
+				parent.left = nf
+				// rebalance current.right
+				nf.rebalanceLeft(parent)
+			} else {
+				// append right
+				nf.right = current // was parent.right
+				nf.left = nil
+				parent.right = nf
+				// rebalance current.left
+			}
+			loop = false
+		}
+	}
 }
 
 // Traverse the subtree anchored at f in pre-order depth-first order.
@@ -44,7 +81,7 @@ func (f *Frame) Traverse(visit func(*Frame) error) error {
 	for !s.empty() {
 		current, err := s.pop()
 		if err != nil {
-			return errors.Wrap(err, "read failed")
+			return errors.Wrap(err, "stack error")
 		}
 		if err = visit(current); err != nil {
 			return err
@@ -60,10 +97,29 @@ func (f *Frame) Traverse(visit func(*Frame) error) error {
 	return nil
 }
 
+// rebalanceLeft re-distributes child nodes so that their addresses are
+// correctly sorted.
+func (f *Frame) rebalanceLeft(parent *Frame) {
+	if f.left == nil || f.left.right == nil {
+		return
+	}
+	// detach affected nodes
+	branch := f.left.right
+	f.left.right = nil
+
+	branch.Traverse(func(tf *Frame) error {
+		fmt.Println("rebalancing ", tf)
+		f.Add(tf, parent)
+		return nil
+	})
+}
+
 // String returns a string representation of the Frame.
 func (f Frame) String() string {
 	return fmt.Sprintf("[%d,%d]", f.Address, f.Length)
 }
+
+//---- CTree -------------------------------------------------------------
 
 // CTree is the cartesian tree containing the free memory segments.
 type CTree struct {
@@ -71,69 +127,45 @@ type CTree struct {
 	Frames int
 }
 
-// New return a new empty tree.
-func New() *CTree {
-	return new(CTree)
+// New return a new tree with an initial root node of the given length
+// and located at address 0.
+func New(length int32) *CTree {
+	return &CTree{
+		root:   &Frame{Address: 0, Length: length},
+		Frames: 1,
+	}
 }
 
-// NewWithLength return a new tree with an initial root node already
-// initialized with the given length and address 0.
-func NewWithLength(length int32) *CTree {
-	ct := New()
-	ct.Add(&Frame{Address: 0, Length: length})
-	return ct
-}
-
-// Add inserts a frame to the tree.
-// TODO() check for overlap of ranges
-func (t *CTree) Add(f *Frame) {
-	if t.root == nil {
-		t.root = f
+// Add inserts a frame to the tree. This function handles cases related to
+// the root of the tree. Once the root cases have been handled, it delegates
+// to the Frame's method with the same name.
+func (t *CTree) Add(nf *Frame) {
+	if nf.Length >= t.root.Length {
+		// root insertion
+		if nf.Address <= t.root.Address {
+			// new is to the left
+			nf.right = t.root
+		} else {
+			// new is to the right
+			nf.left = t.root
+		}
 		t.Frames++
+		t.root = nf
 		return
 	}
 
-	current := t.root
-	parent := (*Frame)(nil)
-
-	loop := true
-
-	for loop {
-		if current != nil && f.Length <= current.Length {
-			// new frame is smaller than current, descend according to address
-			parent = current
-			if f.Address <= current.Address {
-				// descend left
-				current = current.left
-			} else {
-				// descend right
-				current = current.right
-			}
+	// root is larger, add to one of the subtrees
+	if nf.Address <= t.root.Address {
+		if t.root.left == nil {
+			t.root.left = nf
 		} else {
-			// new frame is larger than current or we reached the end: insert here
-			if parent == nil {
-				// root insertion
-				t.root = f
-				// here current points to the old root
-				if current.Address <= f.Address {
-					f.left = current
-				} else {
-					f.right = current
-				}
-			} else {
-				if f.Address <= parent.Address {
-					// append left
-					f.left = parent.left
-					f.right = nil
-					parent.left = f
-				} else {
-					// append right
-					f.right = parent.right
-					f.left = nil
-					parent.right = f
-				}
-			}
-			loop = false
+			t.root.left.Add(nf, t.root)
+		}
+	} else {
+		if t.root.right == nil {
+			t.root.right = nf
+		} else {
+			t.root.right.Add(nf, t.root)
 		}
 	}
 	t.Frames++
--- ctree/ctree_test.go
+++ ctree/ctree_test.go
@@ -11,50 +11,96 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAddFirst(t *testing.T) {
-	tree := New()
+func TestRootAddAboveRight(t *testing.T) {
+	tree := New(50) // [0,50]
+	r := tree.root
 
-	f := &Frame{Address: 0, Length: 100}
+	f := &Frame{Address: 100, Length: 100}
 	tree.Add(f)
 
-	assert.Equal(t, 1, tree.Frames)
-	assert.Equal(t, f, tree.root)
-	assert.Nil(t, f.left)
-	assert.Nil(t, f.right)
-}
-
-func TestAddLeft(t *testing.T) {
-	tree := New()
-
-	f2 := &Frame{Address: 100, Length: 100}
-	tree.Add(f2)
-	f1 := &Frame{Address: 1000, Length: 1000}
-	tree.Add(f1)
-
 	assert.Equal(t, 2, tree.Frames)
-	assert.Equal(t, f1, tree.root)
-	assert.Equal(t, f1.left, f2)
+	assert.Equal(t, f, tree.root)
+	assert.Equal(t, r, tree.root.left)
 	assert.Nil(t, tree.root.right)
 }
 
-func TestAddRight(t *testing.T) {
-	tree := New()
+func TestRootAddAboveLeft(t *testing.T) {
+	tree := New(50) // [0,50]
+	// first add something to the right
+	r := &Frame{Address: 200, Length: 100}
+	tree.Add(r)
+	// now add something to the left
+	f := &Frame{Address: 100, Length: 200}
+	tree.Add(f)
 
-	f2 := &Frame{Address: 2000, Length: 100}
-	tree.Add(f2)
-	f1 := &Frame{Address: 100, Length: 1000}
-	tree.Add(f1)
+	assert.Equal(t, 3, tree.Frames)
+	assert.Equal(t, f, tree.root)
+	assert.Equal(t, r, tree.root.right)
+}
+
+func TestRootAddRight(t *testing.T) {
+	tree := New(100) // [0,100]
+
+	f := &Frame{Address: 200, Length: 50}
+	tree.Add(f)
 
 	assert.Equal(t, 2, tree.Frames)
-	assert.Equal(t, f1, tree.root)
-	assert.Equal(t, f1.right, f2)
+	assert.Equal(t, int32(100), tree.root.Length)
+	assert.Equal(t, f, tree.root.right)
 	assert.Nil(t, tree.root.left)
 }
 
-func TestTraverse(t *testing.T) {
-	tree := New()
+func TestRootAddLeft(t *testing.T) {
+	tree := New(100)        // [0,100]
+	tree.root.Address = 100 // move root to the right
 
-	tree.Add(&Frame{Address: 0, Length: 100})
+	f := &Frame{Address: 0, Length: 50}
+	tree.Add(f)
+
+	assert.Equal(t, 2, tree.Frames)
+	assert.Equal(t, int32(100), tree.root.Length)
+	assert.Equal(t, f, tree.root.left)
+	assert.Nil(t, tree.root.right)
+}
+
+func TestAddLeft(t *testing.T) {
+	tree := New(200)        // [0,200]
+	tree.root.Address = 200 // move root to the right -> [200,200]
+	tree.Add(&Frame{Address: 0, Length: 50})
+	tree.Add(&Frame{Address: 300, Length: 100})
+
+	f := &Frame{Address: 100, Length: 100}
+	tree.Add(f)
+
+	assert.Equal(t, 4, tree.Frames)
+
+	actual := tree.root.left
+	assert.Equal(t, f, actual)
+	assert.Equal(t, int32(100), actual.Length)
+	assert.Equal(t, int32(50), actual.left.Length)
+	assert.Nil(t, actual.right)
+}
+
+func TestAddRight(t *testing.T) {
+	tree := New(300)        // [0,300]
+	tree.root.Address = 100 // move root to the right -> [100,300]
+	tree.Add(&Frame{Address: 0, Length: 50})
+	tree.Add(&Frame{Address: 600, Length: 20})
+
+	f := &Frame{Address: 500, Length: 50}
+	tree.Add(f)
+
+	assert.Equal(t, 4, tree.Frames)
+
+	actual := tree.root.right
+	assert.Equal(t, f, actual)
+	assert.Equal(t, int32(50), actual.Length)
+	assert.Equal(t, int32(20), actual.right.Length)
+	assert.Nil(t, actual.left)
+}
+
+func TestTraverse(t *testing.T) {
+	tree := New(100)
 	tree.Add(&Frame{Address: 500, Length: 300})
 	tree.Add(&Frame{Address: 1000, Length: 80})
 	tree.Add(&Frame{Address: 1500, Length: 200})
@@ -67,3 +113,27 @@ func TestTraverse(t *testing.T) {
 	result := strings.Join(nodes, "")
 	assert.Equal(t, "[500,300][0,100][1500,200][1000,80]", result)
 }
+
+/*
+func NewFrame(address, length int32) *Frame {
+	return &Frame{address, length, nil, nil}
+}
+
+func TestRebalance(t *testing.T) {
+	tree := New(5)
+	tree.Add(NewFrame(250, 20))
+	tree.Add(NewFrame(280, 10))
+	tree.Add(NewFrame(350, 15))
+	tree.Add(NewFrame(470, 35))
+
+	tree.Add(NewFrame(300, 25)) // triggers rebalance
+
+	nodes := make([]string, 0, 10)
+	tree.root.Traverse(func(f *Frame) error {
+		nodes = append(nodes, f.String())
+		return nil
+	})
+	result := strings.Join(nodes, "")
+	assert.Equal(t, "[470,35][300,25][250,20][0,5][280,10][350,15]", result)
+}
+*/
